17a0320faac91511fba677d10a4568e0ede42cd5
Maximiliano <max.rodriguezu@duocuc.cl>
2024-11-18 11:10:28 -0300

    second_commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Archivos en los que Tailwind buscará clases
+  ],
+  theme: {
+    extend: {}, // Aquí puedes extender la configuración predeterminada
+  },
+  plugins: [],
+};
